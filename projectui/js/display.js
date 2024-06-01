6de5ed40d055a67ec5bd059f55fda21d7b08372a
vikash sharma <vikashtiwari9893@gmail.com>
2024-06-01 23:57:46 +0530

    thirteen commit

--- projectui/js/display.js
+++ projectui/js/display.js
@@ -1,6 +1,5 @@
-
-async function dataShow(){
-    let myTable = `
+async function dataShow() {
+  let myTable = `
     <table border="1px" width="600px" bgcolor="lightblue" align="center">
     <tr>
         <th>Employee no</th>
@@ -10,31 +9,27 @@ async function dataShow(){
     </tr>
     `;
 
+  let url = "http://localhost:3000/employess";
 
-    let url="http://localhost:3000/employess";
+  let myobj = await fetch(url);
+  console.log(myobj);
+  let myData = await myobj.json();
+  console.log(myData);
 
-    let myobj =await fetch(url);
-    console.log(myobj);
-    let myData = await myobj.json();
-    console.log(myData);
-
-    myData.map((key)=> {
-        myTable += `
+  myData.map((key) => {
+    myTable += `
         <tr>
-            <td>${key.emplyeeno}</td>
+            <td>${key.employeeno}</td>
             <td>${key.name}</td>
             <td>${key.city}</td>
             <td>${key.salary}</td>
         
         </tr>
         `;
-        
-    });
+  });
 
-    myTable += `</table>` ;
-    document.getElementById("demo").innerHTML=  myTable;
-
-
+  myTable += `</table>`;
+  document.getElementById("demo").innerHTML = myTable;
 }
 
 dataShow();
