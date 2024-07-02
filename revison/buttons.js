12a8e6976a0acff8985a1f06918c370d416d2d1a
vikash sharma <vikashtiwari9893@gmail.com>
2024-07-03 00:48:30 +0530

    Events commit

---
+++ revison/buttons.js
@@ -0,0 +1,35 @@
+
+function display(){
+    document.body.style.backgroundColor="blue";
+    let a=2;
+    let b=3;
+    let c=a+b;
+    document.body.innerHTML=c;
+    
+}
+
+function display1(){
+    let a=2;
+    let b=3;
+    let c=a-b;
+    document.body.innerHTML=c;
+    document.body.style.backgroundColor="red";
+}
+
+function display2(){
+    let a=2;
+    let b=3;
+    let c=a*b;
+    document.body.innerHTML=c;
+    document.body.style.backgroundColor="yellow";
+}
+
+
+
+function display3(){
+    let a=2;
+    let b=3;
+    let c=a/b;
+    document.body.innerHTML=c;
+    document.body.style.backgroundColor="aqua";
+}
